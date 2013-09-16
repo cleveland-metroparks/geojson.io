931bbefe4202c49a9a6693de31ffee5a9c8e3011
Stephen Mather <stephen@smathermather.com>
2013-09-16 15:38:38 -0400

    changed tms to false, as I think the origin=nw flag will set the y axis correctly

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -11,7 +11,7 @@ module.exports = function(context) {
         }, {
             title: 'Satellite',  
             layer: L.tileLayer('http://69.54.58.147:8080/tiles/1.0.0/osm_EPSG900913/{z}/{x}/{y}.jpg?origin=nw', {
-                tms: true })
+                tms: false })
         }, {
             title: 'OSM',  
             layer: L.tileLayer('http://maps1.clemetparks.com/tilestache/tilestache.cgi/basemap/{z}/{x}/{y}.jpg', {
