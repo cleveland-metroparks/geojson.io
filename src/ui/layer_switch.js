a1a63214bdb8052846ef0a12c2e542dba543f22e
Stephen Mather <stephen@smathermather.com>
2013-09-16 13:31:18 -0400

    trying to change over to the /tiles service endpoint with origin=nw set

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -10,7 +10,7 @@ module.exports = function(context) {
             })
         }, {
             title: 'Satellite',  
-            layer: L.tileLayer('http://69.54.58.147:8080/tms/1.0.0/osm_EPSG900913/{z}/{x}/{y}.jpg', {
+            layer: L.tileLayer('http://69.54.58.147:8080/tiles/1.0.0/osm_EPSG900913/{z}/{x}/{y}.jpg?origin=nw', {
                 tms: true })
         }, {
             title: 'OSM',  
