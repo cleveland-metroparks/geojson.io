b286b652b4a71c2a7cfa2ef6002ad42147b43641
Stephen Mather <stephen@smathermather.com>
2014-02-10 16:57:42 -0500

    adding leafon as variable. not yet used in group

--- src/ui/layer_switch.js
+++ src/ui/layer_switch.js
@@ -2,6 +2,9 @@ module.exports = function(context) {
 
     return function(selection) {
 
+        var leafon = L.tileLayer('http://69.54.58.148:8080/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
+                tms: false });
+
         var layers = [{ 
             title: 'Leaf-On',  
             layer: L.tileLayer('http://69.54.58.148:8080/tiles/osm_EPSG900913/{z}/{x}/{y}.png?origin=nw', {
